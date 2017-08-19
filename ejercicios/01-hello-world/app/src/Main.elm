21f3dd77494a5e10cd99f7ac7380f140a46c2e39
Pablo Enrici <penrici@madmobile.com>
2017-08-19 10:37:51 -0300

    swapi test

--- ejercicios/01-hello-world/app/src/Main.elm
+++ ejercicios/01-hello-world/app/src/Main.elm
@@ -1,19 +1,33 @@
 module Main exposing (..)
 
-import Html exposing (Html, div, img, text)
-import Html.Attributes exposing (src)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Http
+import Json.Decode
+import RemoteData
 
 
 ---- MODEL ----
 
 
 type alias Model =
-    {}
+    { planets : RemoteData.WebData (List Planet)
+    }
+
+
+type alias Planet =
+    { name : String
+    , diameter : String
+    }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( {}, Cmd.none )
+    ( { planets = RemoteData.NotAsked
+      }
+    , Cmd.none
+    )
 
 
 
@@ -21,12 +35,38 @@ init =
 
 
 type Msg
-    = NoOp
+    = FetchPlanetsClicked
+    | PlanetsResponse (RemoteData.WebData (List Planet))
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        FetchPlanetsClicked ->
+            ( { model | planets = RemoteData.Loading }, getPlanets )
+
+        PlanetsResponse response ->
+            ( { model | planets = response }, Cmd.none )
+
+
+getPlanets : Cmd Msg
+getPlanets =
+    Http.get "https://swapi.co/api/planets/" responseDecoder
+        |> RemoteData.sendRequest
+        |> Cmd.map PlanetsResponse
+
+
+responseDecoder : Json.Decode.Decoder (List Planet)
+responseDecoder =
+    Json.Decode.field "results"
+        (Json.Decode.list planetDecoder)
+
+
+planetDecoder : Json.Decode.Decoder Planet
+planetDecoder =
+    Json.Decode.map2 Planet
+        (Json.Decode.field "name" Json.Decode.string)
+        (Json.Decode.field "diameter" Json.Decode.string)
 
 
 
@@ -36,8 +76,21 @@ update msg model =
 view : Model -> Html Msg
 view model =
     div []
-        [ img [ src "/logo.svg" ] []
-        , div [] [ text "Your Elm App is working!" ]
+        [ h1 [] [ text "Star Wars planets app" ]
+        , button [ type_ "button", onClick FetchPlanetsClicked ] [ text "Fetch planets!" ]
+        , hr [] []
+        , case model.planets of
+            RemoteData.NotAsked ->
+                p [] [ text "Not asked yet." ]
+
+            RemoteData.Loading ->
+                p [] [ text "Loading planets..." ]
+
+            RemoteData.Failure err ->
+                p [] [ text "There was an error fetching planets!" ]
+
+            RemoteData.Success planets ->
+                ol [] (List.map (\p -> li [] [ text (p.name ++ " - " ++ p.diameter) ]) planets)
         ]
 
 
